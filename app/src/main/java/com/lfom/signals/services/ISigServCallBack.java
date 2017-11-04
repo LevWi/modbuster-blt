ff6c53cb3ebf18e144a937d5d2d3c00636c4fdf4
Lev Fomenko <shadow.of.lion@inbox.ru>
2017-11-04 21:06:33 +0300

    ...

---
+++ app/src/main/java/com/lfom/signals/services/ISigServCallBack.java
@@ -0,0 +1,9 @@
+package com.lfom.signals.services;
+
+/**
+ * Created by gener on 01.11.2017.
+ */
+
+public interface ISigServCallBack {
+    void OnNewData(int i);
+}
---
+++ app/src/main/java/com/lfom/ui/MainActivity.java
@@ -0,0 +1,103 @@
+package com.lfom.ui;
+
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.lfom.modbuster.R;
+import com.lfom.signals.services.BluetoothSPPService;
+import com.lfom.signals.services.ISigServCallBack;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void ttt ( ArrayList<? extends Number> num){
+
+    }
+
+    BluetoothSPPService mService;
+
+    // Обратный вызов Сервиса
+    ISigServCallBack mISigServCallBack = new ISigServCallBack() {
+        @Override
+        public void OnNewData(/*TODO ???? Почему требуется final*/ final int i) {
+            runOnUiThread(
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            ((TextView) findViewById(R.id.textView)).setText(String.valueOf(i));
+                        }
+                    });
+        }
+    };
+
+    private final String LOG_TAG =  this.getClass().getSimpleName();
+
+    boolean mBound = false;
+
+    public BluetoothAdapter mBluetoothAdapter;
+    public final int REQUEST_ENABLE_BT = 10;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Bind to LocalService
+        Intent intent = new Intent(this, BluetoothSPPService.class);
+
+        boolean result = bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+        Log.w(LOG_TAG, "bindService" + (result ? " true" : "false") );
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Unbind from the service
+        if (mBound) {
+            unbindService(mConnection);
+            mBound = false;
+        }
+    }
+
+    public void startCommand(View view) {
+        if (mBound) {
+            mService.setCallback(mISigServCallBack);
+        }
+    }
+
+    /**
+     * Defines callbacks for service binding, passed to bindService()
+     */
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            BluetoothSPPService.LocalBinder binder = (BluetoothSPPService.LocalBinder) service;
+            mService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+    };
+
+}
