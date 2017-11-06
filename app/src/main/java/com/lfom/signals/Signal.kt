68dd6b2e509092a6290223a8207a73f41b92fc70
Lev Fomenko <shadow.of.lion@inbox.ru>
2017-11-07 01:08:34 +0300

    Наброски сигналов.

--- app/src/main/java/com/lfom/signals/Signal.kt
+++ app/src/main/java/com/lfom/signals/Signal.kt
@@ -6,25 +6,52 @@ package com.lfom.signals
  */
 
 
-
-interface RefreshSignalListener{
-    fun update()
+interface RefreshSignalListener {
+    fun update(obj : IDataConversions)
 }
 
+interface IDataConversions {
+    var quality : Quality
 
-enum class SignalType{
+    fun getBool(): Boolean?
+    fun getByte() : Byte?
+    fun getShort(): Short?
+    fun getInt(): Int?
+    fun getFloat(): Float?
+    fun getByteArray(): ByteArray?
+    fun getString() : String?
+
+    /*TODO реализовывать методы c аргументами переменной длины Vararg ?
+    * Наверно все же все по простому
+    * */
+    fun setFromBool(value: Boolean?)
+    fun setFromByte(value: Byte?)
+    fun setFromShort(value: Short?)
+    fun setFromInt(value: Int?)
+    fun setFromFloat(value: Float?)
+    fun setFromByteArray(value: ByteArray?)
+    fun setFromString(value: String?)
+}
+
+enum class Quality(val code : Int){
+    ND(0), // нет данных
+    GOOD(1),
+    BAD(-1)
+}
+
+enum class SignalType {
     BOOL,
+    BYTE,
     SHORT,
-    INT ,
+    INT,
     FLOAT,
     BYTE_ARR,
+    STRING
 }
 
-abstract class Signal(val idx: Int, val mType: SignalType) : ISignalIO
 
-class SignalInt(idx : Int) : Signal(idx, SignalType.INT) {
+abstract class Signal(val idx: Int, val mType: SignalType) : IDataConversions, RefreshSignalListener
 
-}
-
+class SignalInt(idx: Int) : Signal(idx, SignalType.INT)
 
 
