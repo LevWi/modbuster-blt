fc3505f0d3f9e1c12b5375ab8f6da6648aa3dc08
Lev Fomenko <shadow.of.lion@inbox.ru>
2017-11-12 01:45:01 +0300

    Наброски сигналов. Класс Миксер байт

--- app/src/main/java/com/lfom/signals/BytesMixer.kt
+++ app/src/main/java/com/lfom/signals/BytesMixer.kt
@@ -4,6 +4,7 @@ package com.lfom.signals
  * Created by gener on 11.11.2017.
  */
 
+/*todo Нужно сделать правильную проверку шаблона на минимальное значение*/
 class BytesMixer(private val template: IntArray) {
     fun mixBytes(arrIn: ByteArray): ByteArray? {
         if ((template.max() ?: return arrIn) >= arrIn.lastIndex) {
