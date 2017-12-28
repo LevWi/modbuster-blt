3244addeb3e96963334ac3579f63a8e03faff06a
Lev Fomenko <shadow.of.lion@inbox.ru>
2017-12-28 18:02:38 +0300

    Настройка конфига

--- app/src/main/java/com/lfom/ui/ConfigFileFinder.kt
+++ app/src/main/java/com/lfom/ui/ConfigFileFinder.kt
@@ -5,9 +5,15 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-import com.lfom.modbuster.R
 import android.content.Intent
+import android.support.v7.app.AlertDialog
 
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+
+import com.lfom.modbuster.R
+import java.io.IOException
 
 
 class ConfigFileFinder : AppCompatActivity() {
@@ -17,12 +23,37 @@ class ConfigFileFinder : AppCompatActivity() {
         setContentView(R.layout.activity_config_file_finder)
     }
 
-    private val FIND_FILE : Int = 0xA
+    private val FIND_FILE: Int = 0xA
 
-    public fun onFindFile(view : View){
+
+    public fun onFindFile(view: View) {
         val intent = Intent(Intent.ACTION_GET_CONTENT)
-        intent.type = "file/*"
+        intent.type = "application/*"
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
         startActivityForResult(intent, FIND_FILE)
     }
 
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == FIND_FILE && resultCode == Activity.RESULT_OK) {
+            val extFile = File(data?.data?.path ?: return)
+            extFile.name
+            AlertDialog.  //Todo Преобразовать в фрагмент
+            return
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    @Throws(IOException::class)
+    fun copyFile(src: File, dst: File) {
+        val inChannel = FileInputStream(src).channel
+        val outChannel = FileOutputStream(dst).channel
+        try {
+            inChannel?.transferTo(0, inChannel.size(), outChannel)
+        } finally {
+            inChannel?.close()
+            outChannel?.close()
+        }
+    }
 }
